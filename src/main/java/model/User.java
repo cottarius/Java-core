33a305c72dff12e2e235e5153c65ccc3253acfaf
Oleg Prokopenko <cotarius@yandex.ru>
2025-07-23 22:41:23 +0300

    Домашнее задание №4

---
+++ src/main/java/model/User.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.Objects;
+
+public class User {
+
+    private Long id;
+    private String username;
+
+    public User() {}
+
+    public User(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
